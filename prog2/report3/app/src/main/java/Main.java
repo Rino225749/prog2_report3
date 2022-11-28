a2ba989044d7f2a75dc93547842c84a4d9e4a9ee
Rino Higashionna <e225749@ie.u-ryukyu.ac.jp>
2022-11-28 10:10:25 +0900

    4th commit

--- prog2/report3/app/src/main/java/Main.java
+++ prog2/report3/app/src/main/java/Main.java
@@ -12,7 +12,9 @@ public class Main {
             turn++;
             System.out.printf("%dターン目開始！\n", turn);
             hero.attack(enemy);
-            enemy.attack(hero);
+            if(enemy.getDead() == false){
+                enemy.attack(hero);
+            }
         }
         System.out.println("戦闘終了");
     }
